e1ddb28cba42d8c84cf2644b708a363d75dd8c67
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2023-09-26 00:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-//your code here!
-document.addEventListener("DOMContentLoaded", function() {
-    // Function to add list items
-    function addListItems() {
-        const myList = document.getElementById("myList");
+document.addEventListener("DOMContentLoaded", function () {
+  const infiList = document.getElementById("infi-list");
+  let page = 1; // To keep track of the current page
+  const itemsPerPage = 10; // Number of items to load per page
+  let loading = false; // To prevent multiple simultaneous requests
 
-        // Add 10 list items by default
-        for (let i = 1; i <= 10; i++) {
-            const listItem = document.createElement("li");
-            listItem.textContent = "List Item " + i;
-            myList.appendChild(listItem);
-        }
+  // Function to load more items
+  function loadMoreItems() {
+    if (loading) return; // Prevent multiple simultaneous requests
+    loading = true;
 
-        // When the user scrolls to the end of the list, add 2 more list items
-        myList.addEventListener("scroll", function() {
-            if (myList.scrollTop + myList.clientHeight >= myList.scrollHeight) {
-                for (let i = 1; i <= 2; i++) {
-                    const listItem = document.createElement("li");
-                    listItem.textContent = "List Item " + (myList.children.length + i);
-                    myList.appendChild(listItem);
-                }
-            }
-        });
+    // Simulate an API call to fetch more data (you can replace this with a real API call)
+    setTimeout(() => {
+      for (let i = 1; i <= itemsPerPage; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = "Item " + (page * itemsPerPage + i);
+        infiList.appendChild(listItem);
+      }
+      page++; // Increment the page number
+      loading = false;
+    }, 1000); // Simulated delay (replace with actual data fetching)
+  }
+
+  // Initial load of items
+  loadMoreItems();
+
+  // Load more items when user scrolls to the bottom
+  window.addEventListener("scroll", function () {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 200
+    ) {
+      loadMoreItems();
     }
-
-    // Call the function to add list items
-    addListItems();
+  });
 });
-
